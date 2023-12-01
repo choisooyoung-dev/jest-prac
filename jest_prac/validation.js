d7b50bb631e058700bc9de9b0dd0e5a7dcbfb2aa
sooyoung.choi <sooyoung.choi.dev@gmail.com>
2023-12-02 01:38:13 +0900

    ✨ jest test code 구현해보기

---
+++ jest_prac/validation.js
@@ -0,0 +1,66 @@
+// validation.js
+
+// export const isEmail = (value) => {
+//     const email = (value || '');
+  
+//     if (email.split('@').length !== 2) {
+//         return false;
+//     } else if (email.includes(' ')) {
+//         return false;
+//     } else if (email[0] === '-') {
+//         return false;
+//     }
+  
+//     return true;
+//   };
+
+  // validation.js
+
+// export const isEmail = (value) => {
+//     const email = value || "";
+//     const [localPart, domain, ...etc] = email.split("@");
+  
+//     if (!localPart || !domain || etc.length) {
+//       return false;
+//     } else if (email.includes(" ")) {
+//       return false;
+//     } else if (email[0] === "-") {
+//       return false;
+//     }
+  
+//     for (const word of localPart.toLowerCase().split("")) {
+//       if (!["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","-","+","_"].includes(word)) {
+//         return false;
+//       }
+//     }
+  
+//     for (const word of domain.toLowerCase().split("")) {
+//       if (!["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","-", "."].includes(word)) {
+//         return false;
+//       }
+//     }
+  
+//     return true;
+//   };
+
+// 정규표현식 isEmail 함수 디버깅 예시
+// validation.js
+
+export const isEmail = (value) => {
+    const email = value || "";
+    const [localPart, domain, ...etc] = email.split("@");
+  
+    if (!localPart || !domain || etc.length) {
+      return false;
+    } else if (email.includes(" ")) {
+      return false;
+    } else if (email[0] === "-") {
+      return false;
+    } else if (!/^[a-z0-9+_-]+$/gi.test(localPart)) {
+      return false;
+    } else if (!/^[a-z0-9.-]+$/gi.test(domain)) {
+      return false;
+    }
+  
+    return true;
+  };
